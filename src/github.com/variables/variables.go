66bc8331a4bd0742696c2901b4648cf118e9564c
IevgenPr <8950668+IevgenPr@users.noreply.github.com>
2019-12-15 13:26:31 -0800

    Get and Set environment variables.
    This works only within application environment, does not affect other applications

--- src/github.com/variables/variables.go
+++ src/github.com/variables/variables.go
@@ -1,3 +1,4 @@
+// Check this: https://golang.org/doc/code.html
 package main
 
 import (
@@ -44,10 +45,14 @@ func main() {
 	printVar(constString)
 	//cannot do that  - asssign to const - verified by compiler
 	//constString = "test"
-	fmt.Println("\nEnvironment variables:")
-	for _, env := range os.Environ() {
-		fmt.Println(env)
-	}
+	printEnvVars()
+
+	name = os.Getenv("USER")
+	// sets env var for duration of application run
+	os.Setenv("TEST_ENV", "test environment variable")
+	testEnv := os.Getenv("TEST_ENV")
+	printVar(testEnv)
+	printEnvVars()
 }
 
 // function names that start with lowercase - are private within a module
@@ -69,4 +74,12 @@ func changeNameByRef(name *string) string {
 	fmt.Println("name inside: ")
 	printVar(*name)
 	return *name
+}
+
+func printEnvVars() string {
+	fmt.Println("\nEnvironment variables:")
+	for _, env := range os.Environ() {
+		fmt.Println(env)
+	}
+	return ""
 }
