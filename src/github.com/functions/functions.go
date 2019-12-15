89d161898dd04106e25e619b9bb1bba60a7543a4
IevgenPr <8950668+IevgenPr@users.noreply.github.com>
2019-12-15 15:16:20 -0800

    Variadic functions

--- src/github.com/functions/functions.go
+++ src/github.com/functions/functions.go
@@ -10,10 +10,23 @@ func main() {
 	author := "smart guy"
 
 	fmt.Println(convert(name, author))
+
+	bestScore := bestLeagueScore(13, 14, 2, 5, 8, 1)
+	fmt.Println("Best score:", bestScore)
 }
 
 func convert(s1, s2 string) (st1, st2 string) {
 	s1 = strings.Title(s1)
 	s2 = strings.ToUpper(s2)
 	return s1, s2
+}
+
+func bestLeagueScore(finishes ...int) int {
+	best := finishes[0]
+	for _, score := range finishes {
+		if score > best {
+			best = score
+		}
+	}
+	return best
 }
