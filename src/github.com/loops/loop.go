97a446951c66a5bba1740ed44ae5563ac01aab0a
IevgenPr <8950668+IevgenPr@users.noreply.github.com>
2019-12-15 20:11:56 -0800

    nested loops

--- src/github.com/loops/loop.go
+++ src/github.com/loops/loop.go
@@ -4,10 +4,22 @@ import "fmt"
 
 func main() {
 	// slice - unordered list
-	coursesInProg := []string{"Docker deep dive", "Docker Clustering",
+	coursesInProg := []string{
+		"Docker deep dive",
+		"Docker Clustering",
 		"Docker and Kubernetes"}
+	coursesCompleted := []string{
+		"Docker deep dive",
+		"Go fundamentals",
+		"Puppet fundamentals",
+	}
 
-	for _, course := range coursesInProg {
-		fmt.Println(course)
+	for i, course := range coursesInProg {
+		fmt.Println(i, course)
+		for _, completed := range coursesCompleted {
+			if completed == course {
+				fmt.Println("hey, we found a clash:\n", completed)
+			}
+		}
 	}
 }
