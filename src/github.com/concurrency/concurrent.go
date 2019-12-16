6100afc544cab9c9648ff4ce351d54b0a4e1b02a
IevgenPr <8950668+IevgenPr@users.noreply.github.com>
2019-12-16 12:10:28 -0800

    wrap concurrent goroutines in sync package constructs

--- src/github.com/concurrency/concurrent.go
+++ src/github.com/concurrency/concurrent.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
-	func() {
-		time.Sleep(5 * time.Second)
+	var waitGrp sync.WaitGroup
+	waitGrp.Add(2)
+
+	go func() { //goroutine
+		defer waitGrp.Done()
+
+		time.Sleep(5 * time.Second) // blocks execution for 5 sec
 		fmt.Println("Hi there")
 	}() // parens make func self executing
 
-	func() {
+	go func() {
+		defer waitGrp.Done()
+
 		fmt.Println("Niccce")
 	}()
+
+	waitGrp.Wait()
+
 }
