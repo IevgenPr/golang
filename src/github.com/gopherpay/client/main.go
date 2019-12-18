14f3991a80434ff3176397b5b92c2bfb65951174
IevgenPr <8950668+IevgenPr@users.noreply.github.com>
2019-12-18 10:44:53 -0800

    Make use of go interfaces

--- src/github.com/gopherpay/client/main.go
+++ src/github.com/gopherpay/client/main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/gopherpay/payment"
 )
 
 func main() {
-	//var option payment.PaymentOption
-	credit := payment.CreateCreditAccount("Debra Ingram", "1111-2222-3333-4444", 8, 2024, 123)
+	var option payment.PaymentOption
+	option = payment.CreateCreditAccount("Debra Ingram", "1111-2222-3333-4444", 8, 2024, 123)
+	option.ProcessPayment(500)
+	option = payment.CreateCashAccount()
+	option.ProcessPayment(50)
+	// fmt.Printf("Owner name: %s\n", option.OwnerName())
+	// fmt.Printf("Card Number: %s\n", option.CardNumber())
+	// fmt.Println("Trying to change card number")
+	// err := option.SetCardNumber("invalid")
+	// if err != nil {
+	// 	fmt.Printf("Error: %v\n", err)
+	// }
 
-	fmt.Printf("Owner name: %s\n", credit.OwnerName())
-	fmt.Printf("Card Number: %s\n", credit.CardNumber())
-	fmt.Println("Trying to change card number")
-	err := credit.SetCardNumber("invalid")
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-	}
-
-	fmt.Printf("Available Credit: %f\n", credit.AvailableCredit())
+	// fmt.Printf("Available Credit: %f\n", credit.AvailableCredit())
 
 }
