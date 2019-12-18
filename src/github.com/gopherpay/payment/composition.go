5a387d09e610686d6941ce53967c131c4db082a6
IevgenPr <8950668+IevgenPr@users.noreply.github.com>
2019-12-18 15:56:41 -0800

    Composition method conflict resolution

--- src/github.com/gopherpay/payment/composition.go
+++ src/github.com/gopherpay/payment/composition.go
@@ -2,27 +2,37 @@ package payment
 
 import "fmt"
 
-// go composition
+// go hybrid composition
 
-type Account struct{}
+//CreditAccount compositon of account
+type CreditAccount struct{}
 
-func (Account) AvailableFunds() float32 {
+type CheckingAccount struct{}
+
+type HybirdAccount struct {
+	// no need to define a variable, all methods are transparently available
+	CreditAccount //Account is now embedded into the structure
+	CheckingAccount
+}
+
+// AvailableFunds specific to credit account
+func (*CreditAccount) AvailableFunds() float32 {
 	fmt.Println("Listing available funds: ")
 	return 0
 }
 
-func (Account) ProcessPayment(amount float32) bool {
-	fmt.Println("Processing payment")
-	return true
+// AvailableFunds specific for checking account
+func (*CheckingAccount) AvailableFunds() float32 {
+	fmt.Println("Listing available funds: ")
+	return 0
 }
 
-//CreditAccount compositon of account
-type CreditAccount struct {
-	// no need to define a variable, all methods are transparently available
-	Account
+//AvailableFunds specific for hybrid account - otherwise compilation fails
+func (h *HybirdAccount) AvailableFunds() float32 {
+	return h.CreditAccount.AvailableFunds() + h.CheckingAccount.AvailableFunds()
 }
 
 func blah() {
-	ca := &CreditAccount{}
-	funds := ca.AvailableFunds()
+	ha := &HybirdAccount{}
+	fmt.Println(ha.AvailableFunds())
 }
